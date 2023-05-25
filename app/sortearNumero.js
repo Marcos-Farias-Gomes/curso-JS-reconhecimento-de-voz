83ba7bba0ec6b6cbb4e3f346daf3bbce6f9d3650
Marcos-Farias-Gomes <113618017+Marcos-Farias-Gomes@users.noreply.github.com>
2023-05-25 13:49:35 -0300

    index

---
+++ app/sortearNumero.js
@@ -0,0 +1,9 @@
+const menorNumero = 1;
+const maiorNumero = 100;
+const numeroSecreto = gerarNumeroAleatorio();
+
+function gerarNumeroAleatorio(){
+   return parseInt (Math.random() * maiorNumero + 1)
+}
+
+const elementoMenorNumero = document.querySelector()
